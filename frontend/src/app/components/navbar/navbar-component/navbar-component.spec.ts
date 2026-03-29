7e09b2b6ba96d7e0e3ff13d73386d9d693ab67c2
Thimon Pelka <e12123690@student.tuwien.ac.at>
2026-03-29 11:40:51 +0200

    feat(6): added one more test for navbar

--- frontend/src/app/components/navbar/navbar-component/navbar-component.spec.ts
+++ frontend/src/app/components/navbar/navbar-component/navbar-component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AuthService } from '../../../services/auth/auth-service';
+
 import { NavbarComponent } from './navbar-component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
+  let authServiceMock: {
+    logout: ReturnType<typeof vi.fn>;
+  };
+
   beforeEach(async () => {
+    authServiceMock = {
+      logout: vi.fn(),
+    };
+
     await TestBed.configureTestingModule({
       imports: [NavbarComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
@@ -18,5 +29,11 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call auth service on logout', () => {
+    component.logout();
+
+    expect(authServiceMock.logout).toHaveBeenCalled();
   });
 });
