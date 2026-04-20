2fd80a3c2e7204878b079de59142a6e16648352d
c0Brewer <christoph.braeuer66@gmail.com>
2026-04-20 18:30:03 +0200

    feat(30): added pagination and the possibility to query to the TeamHandler.cs file.

--- backend/PayTrack/Api/Handler/TeamHandler.cs
+++ backend/PayTrack/Api/Handler/TeamHandler.cs
@@ -5,6 +5,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using PayTrack.Api.Mapper;
+using PayTrack.Application.Dto.Pagination;
 using PayTrack.Application.Dto.Team;
 using PayTrack.Application.Exceptions;
 using PayTrack.Application.Services.Model;
@@ -19,29 +20,35 @@ namespace PayTrack.Api.Handler
         /// <summary>
         /// Returns all teams.
         /// </summary>
+        /// <param name="query">Query object including all query options.</param>
         /// <param name="teamService">Dependency-Injected Service.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public static async Task<Results<Ok<List<TeamDto>>, BadRequest<ProblemDetails>, ProblemHttpResult>> GetTeamsAsync(
+        public static async Task<Results<Ok<PaginatedResponse<TeamDto>>, BadRequest<ProblemDetails>, ProblemHttpResult>> GetTeamsAsync(
+            [AsParameters] GetTeamQuery query,
             ITeamService teamService)
         {
-            var team = await teamService.GetTeamsAsync();
+            var (teamList, totalCount) = await teamService.GetTeamsAsync(query);
 
-            var teamDto = TeamMapper.ListToDto(team);
+            var teamListDto = TeamMapper.ListToDto(teamList);
 
-            return TypedResults.Ok(teamDto);
+            var paginatedResponse = new PaginatedResponse<TeamDto>(teamListDto, totalCount, query.Limit ?? -1, query.Offset ?? -1);
+
+            return TypedResults.Ok(paginatedResponse);
         }
 
         /// <summary>
         /// Returns a Team by ID.
         /// </summary>
         /// <param name="id">id.</param>
+        /// <param name="query">Query object including all query options.</param>
         /// <param name="teamService">Dependency-Injected Service.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task<Results<Ok<TeamDto>, BadRequest<ProblemDetails>, NotFound<ProblemDetails>, ProblemHttpResult>> GetTeamByIdAsync(
             [FromRoute] int id,
+            [AsParameters] GetTeamQueryById query,
             ITeamService teamService)
         {
-            var team = await teamService.GetTeamByIdAsync(id) ?? throw new NotFoundException("Team could not be found");
+            var team = await teamService.GetTeamByIdAsync(id, query) ?? throw new NotFoundException("Team could not be found");
 
             var createdTeamDto = TeamMapper.ToDto(team);
 
