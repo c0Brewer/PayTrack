7bb6263ac60687b6802fe97e742571aeb62ebb75
c0Brewer <christoph.braeuer66@gmail.com>
2026-04-22 21:42:23 +0200

    feat(31): added the feature to edit/update a single team.

--- frontend/src/app/components/team/team-edit-modal-component/team-edit-modal-component.ts
+++ frontend/src/app/components/team/team-edit-modal-component/team-edit-modal-component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { TeamDto } from '../../../types/exporter';
 import { ModalComponent } from '../../general/modal-component/modal-component';
 
 @Component({
@@ -9,4 +10,46 @@ import { ModalComponent } from '../../general/modal-component/modal-component';
   templateUrl: './team-edit-modal-component.html',
   styleUrl: './team-edit-modal-component.scss',
 })
-export class TeamEditModalComponent {}
+export class TeamEditModalComponent implements OnChanges {
+  @Input() team: TeamDto = {
+    id: -1,
+    name: '',
+    description: '',
+    displayColor: '',
+  };
+
+  @Output() saveEvent = new EventEmitter<TeamDto>();
+  @Output() closeEvent = new EventEmitter<void>();
+
+  originalTeam: TeamDto | null = null;
+
+  ngOnChanges(): void {
+    if (this.team) {
+      // Deep clone to avoid mutating the original reference during editing.
+      this.originalTeam = structuredClone(this.team);
+    }
+  }
+
+  hasTeamBeenChanged(): boolean {
+    if (!this.originalTeam) return false;
+
+    return (
+      this.team.name !== this.originalTeam.name ||
+      this.team.description !== this.originalTeam.description ||
+      this.team.displayColor !== this.originalTeam.displayColor
+    );
+  }
+
+  onSave(): void {
+    if (!this.hasTeamBeenChanged()) {
+      this.onClose();
+      return;
+    }
+
+    this.saveEvent.emit(this.team);
+  }
+
+  onClose(): void {
+    this.closeEvent.emit();
+  }
+}
