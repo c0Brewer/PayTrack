e1005b5a7faf02b04780eaf6b9a57b9870fd8157
Thimon Pelka <e12123690@student.tuwien.ac.at>
2026-04-28 20:40:16 +0200

    fix(45): fixed build issues

--- frontend/src/app/components/home/home-component/home-component.ts
+++ frontend/src/app/components/home/home-component/home-component.ts
@@ -1,23 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { NotificationService } from '../../../services/notification/notification-service';
 import { BoxComponent } from '../../general/boxes/box-component/box-component';
-import { NavbarComponent } from '../../navbar/navbar-component/navbar-component';
 
 @Component({
   selector: 'app-home-component',
-  imports: [CommonModule, NavbarComponent, BoxComponent],
+  imports: [CommonModule, BoxComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   private greetings = [
     'Willkommen zurück!',
     'Schön, dich wiederzusehen!',
     '    Schön, dass du wieder da bist!',
   ];
-  constructor(private readonly notificationService: NotificationService) {}
 
   getGreeting(): string {
     return this.greetings[Math.floor(Math.random() * this.greetings.length)];
