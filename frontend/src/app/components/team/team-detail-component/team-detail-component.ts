92dfd4bcdec0e089578cdf9490a962171a3297c7
c0Brewer <christoph.braeuer66@gmail.com>
2026-05-02 00:29:51 +0200

    feat(31): add linked team detail budget cost centres

--- frontend/src/app/components/team/team-detail-component/team-detail-component.ts
+++ frontend/src/app/components/team/team-detail-component/team-detail-component.ts
@@ -1,20 +1,22 @@
 import { SlicePipe } from '@angular/common';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
+import { CostCentreService } from '../../../services/cost-centre/cost-centre-service';
 import { NotificationService } from '../../../services/notification/notification-service';
 import { TeamService } from '../../../services/team/team-service';
-import { TeamDto } from '../../../types/exporter';
+import { CostCentreDto, TeamDto } from '../../../types/exporter';
 
 @Component({
   selector: 'app-team-detail-component',
-  imports: [SlicePipe],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './team-detail-component.html',
   styleUrl: './team-detail-component.scss',
 })
 export class TeamDetailComponent implements OnInit {
   constructor(
     private readonly teamService: TeamService,
+    private readonly costCentreService: CostCentreService,
     private readonly notificationService: NotificationService,
     private readonly route: ActivatedRoute,
     private readonly router: Router,
@@ -22,8 +24,11 @@ export class TeamDetailComponent implements OnInit {
   ) {}
 
   team: TeamDto | null = null;
+  costCentres: CostCentreDto[] = [];
 
   ngOnInit(): void {
+    this.loadCostCentres();
+
     this.route.paramMap.subscribe((params) => {
       const id = Number(params.get('id'));
 
@@ -45,5 +50,24 @@ export class TeamDetailComponent implements OnInit {
 
   formatBudgetAmount(amount: number): string {
     return new Intl.NumberFormat('de-DE', { maximumFractionDigits: 2 }).format(amount);
+  }
+
+  getCostCentreName(costCentreId: number): string {
+    return (
+      this.costCentres.find((costCentre) => costCentre.id === costCentreId)?.name ??
+      `Cost Centre #${costCentreId}`
+    );
+  }
+
+  private loadCostCentres(): void {
+    this.costCentreService.getCostCentres({ Limit: 1000, Offset: 0 }).subscribe({
+      next: (data) => {
+        this.costCentres = data.items ?? [];
+        this.cdr.detectChanges();
+      },
+      error: (err: Error) => {
+        this.notificationService.showError('Could not load cost centres: ' + err.message);
+      },
+    });
   }
 }
