44a818061faf204dbe4ef6532cd1736ba3b8ddcb
Thimon Pelka <e12123690@student.tuwien.ac.at>
2026-05-07 15:14:31 +0200

    fix(14): fixed lint

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,9 +7,9 @@ import { CostCentreManagementComponent } from './components/cost-centre/cost-cen
 import { UnauthorizedComponent } from './components/general/unauthorized-component/unauthorized-component';
 import { HomeComponent } from './components/home/home-component/home-component';
 import { LoginComponent } from './components/login/login-component/login-component';
+import { SettingsComponent } from './components/settings/settings-component/settings-component';
 import { MyInvoiceDetailComponent } from './components/submission/my-invoice-detail-component/my-invoice-detail-component';
 import { MyInvoicesComponent } from './components/submission/my-invoices-component/my-invoices-component';
-import { SettingsComponent } from './components/settings/settings-component/settings-component';
 import { ReceiptOverviewComponent } from './components/submission/receipt-overview-component/receipt-overview-component';
 import { ReceiptSubmitComponent } from './components/submission/receipt-submit-component/receipt-submit-component';
 import { TeamDetailComponent } from './components/team/team-detail-component/team-detail-component';
