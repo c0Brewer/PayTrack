b5bd2134d091bfaa9fd7a7476e0f95e2b2e4809e
Jakob <jakobschreib01@gmail.com>
2026-05-21 19:58:58 +0200

    fix(79): format frontend with just

--- frontend/src/app/components/submission/duplicate-list-modal-component/duplicate-list-modal-component.ts
+++ frontend/src/app/components/submission/duplicate-list-modal-component/duplicate-list-modal-component.ts
@@ -1,10 +1,7 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import {
-  DuplicatePaymentRequestByUserDto,
-  PaymentRequestByUserDto,
-} from '../../../types/exporter';
+import { DuplicatePaymentRequestByUserDto, PaymentRequestByUserDto } from '../../../types/exporter';
 import { ModalComponent } from '../../general/modal-component/modal-component';
 
 @Component({
