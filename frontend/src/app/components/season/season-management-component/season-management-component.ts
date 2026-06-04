3d270fca1c4716602784ebf7dbb968ec1100741a
jakobjeschke <jakob.jeschke@tuwien.ac.at>
2026-06-04 18:32:43 +0200

    style(88): season management restyling, WIP

--- frontend/src/app/components/season/season-management-component/season-management-component.ts
+++ frontend/src/app/components/season/season-management-component/season-management-component.ts
@@ -5,10 +5,11 @@ import { SeasonService } from '../../../services/season/season-service';
 import { SeasonDto } from '../../../types/exporter';
 import { SeasonFormComponent } from '../season-form-component/season-form-component';
 import { SeasonListComponent } from '../season-list-component/season-list-component';
+import { BoxComponent } from '../../general/boxes/box-component/box-component';
 
 @Component({
   selector: 'app-season-management-component',
-  imports: [SeasonFormComponent, SeasonListComponent],
+  imports: [SeasonFormComponent, SeasonListComponent, BoxComponent],
   templateUrl: './season-management-component.html',
   styleUrl: './season-management-component.scss',
 })
