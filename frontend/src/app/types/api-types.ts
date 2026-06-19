46b44233fa8ba9669e393e27917b85710f9c9b5d
Jakob <jakobschreib01@gmail.com>
2026-06-19 20:46:32 +0200

    fix(116): formated frontend and backend and generated api

--- frontend/src/app/types/api-types.ts
+++ frontend/src/app/types/api-types.ts
@@ -1668,48 +1668,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/api/v1/transaction/user/receipt/extract": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get?: never;
-        put?: never;
-        post: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: {
-                content: {
-                    "multipart/form-data": {
-                        /** Format: binary */
-                        receipt: string;
-                    };
-                };
-            };
-            responses: {
-                /** @description OK */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["ReceiptExtractionDto"];
-                    };
-                };
-            };
-        };
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
     "/api/v1/transaction/user/{id}/mark-paid": {
         parameters: {
             query?: never;
@@ -2707,8 +2665,6 @@ export interface components {
             receipt: string;
             payoutType: components["schemas"]["PayoutType"];
             creditorName?: string | null;
-            /** Format: date-time */
-            dueDate?: string | null;
             /** Format: int32 */
             bankAccountId?: number | null;
         };
@@ -2746,18 +2702,6 @@ export interface components {
             paidAt: string;
             /** Format: int32 */
             budgetId?: number | null;
-        };
-        DateTimeNullableExtractedReceiptFieldDto: {
-            /** Format: date-time */
-            value?: string | null;
-            /** Format: double */
-            confidence?: number;
-        };
-        DecimalNullableExtractedReceiptFieldDto: {
-            /** Format: double */
-            value?: number | null;
-            /** Format: double */
-            confidence?: number;
         };
         DeclinePaymentRequestByUserDto: {
             reason: string;
@@ -2870,8 +2814,6 @@ export interface components {
             hasPotentialDuplicate?: boolean;
             bankAccount?: components["schemas"]["BankAccountDto"];
             creditorName?: string | null;
-            /** Format: date-time */
-            dueDate?: string | null;
         };
         PaymentRequestByUserDtoPaginatedResponse: {
             items: components["schemas"]["PaymentRequestByUserDto"][] | null;
@@ -2899,13 +2841,6 @@ export interface components {
         } & {
             [key: string]: unknown;
         };
-        ReceiptExtractionDto: {
-            extractionSucceeded?: boolean;
-            message?: string | null;
-            amount?: components["schemas"]["DecimalNullableExtractedReceiptFieldDto"];
-            invoiceDate?: components["schemas"]["DateTimeNullableExtractedReceiptFieldDto"];
-            invoiceNumber?: components["schemas"]["StringExtractedReceiptFieldDto"];
-        };
         RequestChangesPaymentRequestByUserDto: {
             reason: string;
         };
@@ -2930,11 +2865,6 @@ export interface components {
             /** Format: email */
             recipientEmail: string;
             message: string;
-        };
-        StringExtractedReceiptFieldDto: {
-            value?: string | null;
-            /** Format: double */
-            confidence?: number;
         };
         TeamDto: {
             /** Format: int32 */
