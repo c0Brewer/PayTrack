f2ec2f9c48e767f13b5a16392708064597887481
jakobjeschke <jakob.jeschke@tuwien.ac.at>
2026-06-21 12:24:05 +0200

    style(106): linting format

--- frontend/src/app/components/payment-requests/request-by-user/user-view/filter-component/filter-component.ts
+++ frontend/src/app/components/payment-requests/request-by-user/user-view/filter-component/filter-component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { debounceTime, Subject } from 'rxjs';
 
